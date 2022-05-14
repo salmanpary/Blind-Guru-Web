fd40e68217b113058492f96a7ccfe0c57ad501e8
jaison080 <jaisondennis080@gmail.com>
2022-05-15 00:47:59 +0530

    Landing UI Updated

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,12 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import LandingPage from './LandingPage'
 
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+      <LandingPage/>
     </div>
   )
 }
