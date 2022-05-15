e3b51ad574e0b9d6f56d7e29242d006a38f94d2c
Mayon Francis <mayonfrancis@gmail.com>
2022-05-15 13:46:15 +0530

    logout

--- blind-guru/pages/api/auth/twitter/logout.js
+++ blind-guru/pages/api/auth/twitter/logout.js
@@ -1,7 +1,7 @@
 import { getSession } from "lib/get-session.js";
+import { getCookie } from 'cookies-next';
 
 export default async function handler (req, res) {
-    const session = await getSession(req, res);
-    await session.destroy();
-    res.redirect('/home');
+    removeCookies('user', { req, res });
+    res.redirect('/');
 }
