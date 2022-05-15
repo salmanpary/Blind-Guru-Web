9cc83e4041ba9bd06bb9e4de124ff3308793a6c4
jaison080 <jaisondennis080@gmail.com>
2022-05-15 11:46:12 +0530

    UI Updated

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -6,9 +6,8 @@ function LandingPage()
     return(
         < div className="landing">
             <div className="heading">Blind-Guru</div>
-            <button className="login-btn" onClick={Login}><a>Login With Twitter</a></button>
+            <button className="login-btn" onClick={Login}><i class="fa fa-twitter"></i>  Login With Twitter</button>
             <div className="description">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
-            <div className="footer">Made with ❤️ by Team Fin-eazy</div>
         </div>
     )
 }
